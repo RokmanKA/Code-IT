2e67a11538d3393336c74472045717573d263fb8
RokmanKA <roman.kazmirishin@icloud.com>
2022-04-16 21:32:38 +0300

    sss

---
+++ lesson5.JS/homework1+/main.js
@@ -0,0 +1,42 @@
+let height = 23;
+let width = 10;
+let s = (height * width);
+
+console.log(s + ' ' + 'sm');
+
+///////////////////////////////////////////////////
+
+
+const heightC = 10;
+const dc = 4;
+const v = `${Math.PI * Math.pow(dc/2, 2) *  heightC}m`;
+
+console.log(v);
+
+
+
+///////////////////////////////////////////////////////////
+
+let n = 3;
+let m = 4;
+let k = Math.sqrt(Math.pow(n,2) + Math.pow(m,2));
+
+console.log(k);
+
+///////////////////////////////////////////////////////////////
+
+
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 === 6);
+console.log(10 === 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 < 10);
+console.log(10 != 10);
+console.log(123 ==='123');
+console.log(123 == '123');
+
+
---
+++ lesson5.JS/homework1/main.js
@@ -0,0 +1,107 @@
+// let a1 = -999;
+// let a2 = 123;
+// let a3 = 'owu';
+// let a4 = 'hello';
+// let a5 = 'com';
+// let a6 = 'ua';
+// let a7 = 1;
+// let a8 = 10;
+// let a9 = 3.1;
+// let a10 = 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+//
+//
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+// console.log(a5)
+// console.log(a6)
+// console.log(a7)
+// console.log(a8)
+// console.log(a9)
+// console.log(a10)
+// console.log(a11)
+//
+// alert(a12)
+// document.write(a13)
+//
+// ///////////////////////////////////////////////////
+//
+// let book1 = {
+//     title: 'bob',
+//     pages: 23,
+//     ganre: 'topik'
+// };
+//
+// console.log(book1);
+//
+// //////////////////////////////////////////////////
+//
+//
+// let book2 = {
+//     title: 'veb',
+//     pages: '455',
+//     ganre: 'lol',
+//     autors: [
+//         'ponchik',
+//         'chahlik',
+//         'booblik'
+//     ]
+// };
+//
+// console.log(book2);
+
+///////////////////////////////////////////////////
+
+// let firstName = 'Roman';
+// let middleName = 'Olegovich';
+// let lastName = 'Kazmyryshyn';
+// let person = firstName + ' ' + middleName + ' ' + lastName;
+//
+// console.log(person);
+
+// let firstName = 'Roman';
+// let middleName = 'Olegovich';
+// let lastName = 'Kazmyryshyn';
+// let person1 = `${firstName} ${middleName} ${lastName}`;
+//
+// console.log(person1);
+
+//////////////////////////////////////////////////////////
+
+// let firstName = prompt( 'enter you first name');
+// let middleName = prompt(  'enter you middle name');
+// let lastName = prompt(  'enter you last name');
+//
+// let person = `${firstName} ${middleName} ${lastName}`;
+//
+// console.log(person);
+
+//////////////////////////////////////////////////////////
+
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+
+//////////////////////////////////////////////////////////////
+
+
+let books = [
+    {title: 'bob', pages:10,genre:'drama',autor:['asd','web', 'ford'] },
+    {title: 'pab', pages:20,genre:'fantasy',autor:['asd','web', 'ford'] },
+    {title: 'lol', pages:30,genre:'drama',autor:['asd','web', 'ford'] },
+    {title: 'hah', pages:40,genre:'fantasy',autor:['asd','web', 'ford'] },
+];
+
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
+console.log(books[3]);
