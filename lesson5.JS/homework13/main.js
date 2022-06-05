bf08aa12e5ac552896aeaeafa4b8d688dcc9411f
RokmanKA <roman.kazmirishin@icloud.com>
2022-06-05 14:43:56 +0300

    sss

--- lesson5.JS/homework13/main.js
+++ lesson5.JS/homework13/main.js
@@ -1,106 +1,219 @@
-// console.log(1)
+// let energy = 1000;
 //
-// setTimeout(() => {
-//   console.log(2)
-// }, 0);
+// function wakeUp(energy) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             energy -= 500;
+//             if (energy <= 0) {
+//                 reject('Я не прокинувся');
+//                 return;
+//             }
+//             console.log('Прокинувся');
+//             resolve(energy);
+//         },2000)
+//     })
+// }
 //
-// console.log(3)
+// function eat(energy) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             energy += 200;
+//             console.log('Поїв');
+//             resolve(energy);
+//         },1500)
+//     })
+// }
 //
-// for (let i = 0; i < 1000; i++) {
-//   console.log(i)
+// function goToWork(energy) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             energy -= 500;
+//             if (energy <= 0) {
+//                 reject('не дійшов');
+//                 return;
+//             }
+//             console.log('дійшов');
+//             resolve(energy);
+//         },1000)
+//     })
+// }
+
+// wakeUp(energy)
+//     .then(value => {
+//         console.log(`'енергія' ${value}`)
+//         console.log('------------');
+//         return eat(value);
+//     })
+//     .then(value => {
+//         console.log(`'енергія' ${value}`);
+//         console.log('------------');
+//         return goToWork(value);
+//     })
+//     .then(value => {
+//         console.log(`енергія ${value}`);
+//         console.log('------------');
+//
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     })
+
+
+// let energy = 100;
+//
+// function wakeUp(energy) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             energy -= 500;
+//             if (energy <= 0) {
+//                 reject('Я не прокинувся');
+//                 return;
+//             }
+//             resolve(energy);
+//         },2000)
+//     })
+// }
+//
+// function eat(energy) {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             energy += 200;
+//             resolve(energy);
+//         },1500)
+//     })
+// }
+//
+// function goToWork(energy) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             energy -= 500;
+//             if (energy <= 0) {
+//                 reject('не дійшов');
+//                 return;
+//             }
+//             resolve(energy);
+//         },1000)
+//     })
 // }
 //
 //
-//
-//
-// document.getElementById('btn').onclick = () => {
-//   console.log('clc');
+// function message(msg, data) {
+//     console.log(msg);
+//     console.log(`залишилось ${data}`);
+//     console.log('----------');
 // }
 //
-// let i = 0
-// while (i < 999999999) {
-//   i++;
+// async function start(energy) {
+//     try {
+//        let data = await wakeUp(energy);
+//        message('прокинувся', data);
+//
+//        data = await eat(data);
+//        message('Поїв', data);
+//
+//        data = await goToWork(data);
+//        message('Дійшов', data);
+//     }
+//     catch (err) {
+//         console.log(err);
+//     }
+// }
+// start(energy);
+
+// let energy = 1000;
+// function wakeUp(energy, cb) {
+//     setTimeout(() => {
+//         energy -= 500;
+//         if (energy <= 0) {
+//             cb('Не прокинувся', null);
+//             return;
+//         }
+//         console.log('Прокинувся');
+//         cb(null, energy);
+//     },2000);
+// }
+//
+// function eat(energy, cb) {
+//     setTimeout(() => {
+//         energy += 300;
+//         console.log('Поїв');
+//         cb(null, energy);
+//     },1000);
+// }
+//
+// function goToWork(energy, cb) {
+//     setTimeout(() => {
+//         energy -= 500;
+//         if (energy <= 0) {
+//             cb('Не дійшов', null);
+//             return;
+//         }
+//         console.log('Дійшов');
+//         cb(null, energy);
+//     },500);
+// }
+//
+// wakeUp(energy, (err,data) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(`енергія ${data}`);
+//     console.log('----------');
+//
+//     eat(energy, (err, data) => {
+//         if (err) {
+//             console.log(err);
+//             return;
+//         }
+//         console.log(`енергія ${data}`);
+//         console.log('----------');
+//
+//         goToWork(energy, (err, data) => {
+//             if (err) {
+//                 console.log(err);
+//                 return;
+//             }
+//             console.log(`енергія ${data}`);
+//             console.log('----------');
+//         })
+//     })
+// })
+
+// При виділені сегменту тексту на сторінці він стає жирний/курсивний/або якось іншим способом змінює свій стан
+
+// let bold = document.getElementsByClassName('bold')[0];
+// bold.onmouseup = function () {
+//     let select = document.getSelection().toString();
+//     bold.innerHTML = bold.innerText.replace(select, `<b>${select}</b>`);
 // }
 
-// function dead() {
-//   return dead();
-// }
-//
-// dead();
+let one = new Promise(resolve => {
+    setTimeout(() => {
+        resolve(1);
+    },2000);
+})
 
-function goWork(isJobComplete) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (isJobComplete) {
-                resolve(999);
-            } else {
-                reject('No work no money');
-            }
-        }, 100)
-    });
-}
+let two = new Promise((resolve,reject) => {
+    setTimeout(() => {
+        reject(2);
+    },1000);
+})
 
-function goToEgypt(moneyForTour) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (moneyForTour < 450) {
-                reject('Not enough money')
-            } else {
-                console.log('HOOORAY')
-                resolve(moneyForTour - 450);
-            }
-        }, 400)
-    })
-}
+let three = new Promise(resolve => {
+    setTimeout(() => {
+        resolve(3);
+    },500);
+})
 
-function buySouvenirs(money, souvenirCount) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const totalPrice = souvenirCount * 10;
-            if (totalPrice > money) {
-                reject('Not enough');
-            } else {
-                resolve(`Here you go. ${souvenirCount} items`)
-            }
-        }, 300)
-    })
-}
+// Promise.race([one, two, three]).then(value => {
+//     console.log(value);
+// })
 
-// goWork(true)
-//   .then((money) => {
-//     console.log('Good job. Here you go', money, '$');
-//
-//     return goToEgypt(money);
-//   })
-//   .then(moneyAfterTour => {
-//     console.log('It was great vacation', moneyAfterTour);
-//
-//     return buySouvenirs(moneyAfterTour, 105)
-//   })
-//   .then(res => {
-//     console.log(res);
-//   })
-//   .catch(reason => {
-//     console.log('OOOOPS', reason);
-//   })
-//   .finally(() => {
-//     console.log('FINAL')
-//   })
+// Promise.all([one, two, three]).then(value => {
+//     console.log(value);
+// })
 
-
-async function workation() {
-    try {
-        const money = await goWork(false);
-        console.log('Good job. Here you go', money, '$');
-
-        const moneyAfterTour = await goToEgypt(money);
-        console.log('It was great vacation', moneyAfterTour);
-
-        const res = await buySouvenirs(moneyAfterTour, 15);
-        console.log(res);
-    } catch (e) {
-        console.warn('Hoolly ****', e)
-    }
-}
-
-workation()
+Promise.allSettled([one, two, three]).then(value => {
+    console.log(value);
+})
